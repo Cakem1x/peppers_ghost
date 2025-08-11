4017a8bcbf5b51c562ad043a88c783a9940fe6f5
Matthias Holoch <mholoch@gmail.com>
2025-08-11 16:39:20 +0200

    refactor js logic

--- js/cam_analyzer.js
+++ js/cam_analyzer.js
@@ -1,28 +1,67 @@
-let video = document.getElementById('video');
-let canvas = document.getElementById('canvasOutput');
-let ctx = canvas.getContext('2d');
+const ui = {
+    button_start_stop: document.getElementById('button_start_stop'),
+    canvas_segmented: document.getElementById('canvas_segmented_output'),
+    canvas_segmented_context: document.getElementById('canvas_segmented_output').getContext('2d'),
+    video_input: document.getElementById("video_input"),
+    hue1_min: document.getElementById('hue1_min'),
+    hue1_max: document.getElementById('hue1_max'),
+};
+
+let vid_capture_state = {
+    streaming: false,
+};
 
 cv['onRuntimeInitialized'] = async () => {
-  try {
-    let stream = await navigator.mediaDevices.getUserMedia({ video: true });
-    video.srcObject = stream;
-
-    let cap = new cv.VideoCapture(video);
-    let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-    let gray = new cv.Mat(video.height, video.width, cv.CV_8UC1);
-    let edges = new cv.Mat(video.height, video.width, cv.CV_8UC1);
-
-    function processVideo() {
-      cap.read(src);
-      cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY);
-      cv.Canny(gray, edges, 50, 150);
-      cv.imshow('canvasOutput', edges);
-      requestAnimationFrame(processVideo);
-    }
-
-    requestAnimationFrame(processVideo);
-  } catch (err) {
-    console.error("Error accessing camera:", err);
-    alert("Could not access camera: " + err.message);
-  }
+    ui.button_start_stop.removeAttribute('disabled');
 };
+
+function on_video_started() {
+    vid_capture_state.streaming = true;
+    ui.button_start_stop.innerText = 'Stop';
+    segmentation_state.timer = setTimeout(process_video_hsv_segmentation, 0);
+}
+
+ui.button_start_stop.addEventListener('click', () => {
+    if (!vid_capture_state.streaming) {
+        // setup video stream
+        navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+            .then(function(stream) {
+                ui.video_input.srcObject = stream;
+                ui.video_input.play();
+                vid_capture_state.stream = stream;
+                ui.video_input.addEventListener('canplay', on_video_started, false);
+            })
+            .catch(function(err) {
+                console.error('Camera Error: ' + err.name + ' ' + err.message);
+            });
+    } else {
+        ui.video_input.pause();
+        ui.video_input.srcObject = null;
+        ui.video_input.removeEventListener('canplay', on_video_started);
+        if (vid_capture_state.stream) {
+            vid_capture_state.stream.getVideoTracks()[0].stop();
+        }
+        vid_capture_state.streaming = false;
+        ui.canvas_segmented_context.clearRect(0, 0, ui.canvas_segmented.width, ui.canvas_segmented.height);
+        ui.button_start_stop.innerText = 'Start';
+        segmentation_state.timer = null;
+    }
+});
+
+let segmentation_state = {
+    timer: null,
+    fps: 30,
+};
+
+function process_video_hsv_segmentation() {
+    const begin = Date.now();
+    ui.canvas_segmented_context.drawImage(ui.video, 0, 0, ui.canvas_segmented.width, ui.canvas_segmented.height);
+    src.data.set(ui.canvas_segmented_context.getImageData(0, 0, ui.canvas_segmented.width, ui.canvas_segmented.height).data);
+    dst = cv.Mat();
+    cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+    cv.imshow("canvas_segmented_output", dst);
+
+    // schedule next function call
+    const delay = 1000/fps - (Date.now() - begin);
+    segmentation_state.timer = setTimeout(process_video_hsv_segmentation, delay);
+}
